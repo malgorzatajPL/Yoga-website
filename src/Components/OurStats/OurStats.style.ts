fd87a32173992ae187c99a58d5096f6c99ba7a37
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2023-01-28 22:46:13 +0000

    Add before background img

--- src/Components/OurStats/OurStats.style.ts
+++ src/Components/OurStats/OurStats.style.ts
@@ -6,6 +6,20 @@ export const OurStatsStyle = styled.section`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  position: relative;
+  z-index: 0;
+  overflow: hidden;
+  :before {
+    content: '';
+    position: absolute;
+    background-image: url(/Design.png);
+    width: 700px;
+    height: 700px;
+    background-repeat: no-repeat;
+    z-index: -1;
+    left: -48px;
+    top: -6%;
+  }
 `;
 
 export const CardsCircle = styled.div`
